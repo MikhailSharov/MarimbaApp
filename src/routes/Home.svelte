4d6d5ddeb7cde1c0edafa4c8b670e80c8c4ecbce
MIkhail Sharov <36767270+MikhailSharov@users.noreply.github.com>
2022-06-29 10:36:35 -0400

    Sign in continuity until logout and then secure path

---
+++ src/routes/Home.svelte
@@ -0,0 +1,26 @@
+<script>
+  let hello = "Hello World";
+
+  import {getAuth, onAuthStateChanged, signOut } from "firebase/auth";
+
+  
+  const auth = getAuth();
+  const user = auth.currentUser;
+
+  let uid = user.uid;
+
+  function handleSignOut(val) {
+    if (val == true) {
+      signOut(auth).then(() => {
+        console.log("Signed Out");
+      }).catch((error) => {
+      });
+    }
+  }
+
+</script>
+
+<div>
+  <p> Hello {uid}! </p>
+  <button on:click={() => handleSignOut(true)} type="button">Log Out</button>
+</div>
